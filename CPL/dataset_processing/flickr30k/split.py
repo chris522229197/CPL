d36c7449aea91b14c67aed7709c3f6f0677f7992
Chris Lin <chris522229197@gmail.com>
2023-03-09 17:27:56 -0800

    Name flickr30k files with number of shots

--- CPL/dataset_processing/flickr30k/split.py
+++ CPL/dataset_processing/flickr30k/split.py
@@ -32,7 +32,7 @@ for i in range(len(df_test)):
         test_captions.append(df_test.images.str['sentences'].tolist()[i][j]['raw'][:265]) # truncate at char 265
 
 # n-shot train + 5000 test
-textfile = open("train.txt", "w")
+textfile = open(f"train-{FEW_SHOT}shots.txt", "w")
 for i in range(len(train_imnames)):
     textfile.write(train_imnames[i] + "*" + train_captions[i] + "\n")
 for i in range(len(test_imnames)):
@@ -40,18 +40,18 @@ for i in range(len(test_imnames)):
 textfile.close()
 
 # n_shot train + 1000 test 
-textfile = open("test.txt", "w")
+textfile = open(f"test-{FEW_SHOT}shots.txt", "w")
 for i in range(len(train_imnames)):
     textfile.write(train_imnames[i] + "*" + train_captions[i] + "\n")
 for i in range(1000):
     textfile.write(test_imnames[i*5] + "*" + test_captions[i*5] + "\n")
 textfile.close()
 
-textfile = open("val.txt", "w")
+textfile = open(f"val-{FEW_SHOT}shots.txt", "w")
 textfile.close()
 
 # file with all captions
-textfile = open("classnames.txt", "w")
+textfile = open(f"classnames-{FEW_SHOT}shots.txt", "w")
 for i in range(len(train_captions)):
     textfile.write(train_captions[i] + "\n")
 for i in range(len(test_captions)):
